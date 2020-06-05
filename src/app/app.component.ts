fdac773f0e13eaddb366c91948e98b0ebcf8bd6a
DhifMlak <Dhifmlak@gmail.com>
2020-06-05 12:14:24 +0200

    added outgoing/icoming client logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Decathlon';
+  clients: number;
+  clientsForm = new FormGroup({
+    time: new FormControl(20),
+    initialClients: new FormControl(10),
+    inComingClients: new FormControl(2),
+    outGoingClients: new FormControl(1),
+    inComingRatio: new FormControl(1),
+    outGoingRatio: new FormControl(1.5),
+
+  });
+
+  getClientsNumber() {
+    this.clients = ((this.clientsForm.value.time * this.clientsForm.value.inComingClients) / this.clientsForm.value.inComingRatio)
+      - ((this.clientsForm.value.time * this.clientsForm.value.outGoingClients) / this.clientsForm.value.outGoingRatio)
+      + ((this.clientsForm.value.initialClients / this.clientsForm.value.outGoingRatio));
+  }
+
+
 }
